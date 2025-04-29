908935d63092354889b26bc216792639c6a052ab
dung03BE <dung2003hn@gmail.com>
2025-04-29 22:21:32 +0700

    'u

--- src/main/java/com/dung/UniStore/config/WebConfig.java
+++ src/main/java/com/dung/UniStore/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001")  // Thêm origin của frontend vào đây
+                .allowedOrigins("https://fe-unistore.onrender.com")  // Thêm origin của frontend vào đây
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)  // Cho phép gửi credentials
                 .allowedHeaders("*");
