478cb1f9e68d71e1997c99440769d91ad149429d
dung03BE <dung2003hn@gmail.com>
2025-04-29 15:27:57 +0700

    u

--- src/main/java/com/dung/UniStore/config/WebConfig.java
+++ src/main/java/com/dung/UniStore/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+                .addResourceLocations("file:uploads/");
 
     }
     @Override
